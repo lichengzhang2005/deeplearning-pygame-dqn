fa19c49e1028c15f51116d5a80d84b3b53e8de54
leo <leo@futunn.com>
2018-07-23 16:59:12 +0800

    modify version

--- dqn/d.py
+++ dqn/d.py
@@ -1,14 +1,20 @@
 # -*- coding: utf-8 -*-
+import tensorflow as tf
 import random
 import gym
 import numpy as np
 from collections import deque
-from tensorflow.tools.api.generator.api.keras.models import Sequential
-from tensorflow.tools.api.generator.api.keras.layers import Dense
-from tensorflow.tools.api.generator.api.keras.optimizers import Adam
 from game import Game
 
-EPISODES = 1000
+# from tensorflow.keras import Sequential # import Sequential
+# from tf.keras.layers import Dense
+# from tf.keras.optimizers import Adam
+
+Sequential = tf.keras.Sequential
+Dense = tf.keras.layers.Dense
+Adam = tf.keras.optimizers.Adam
+
+EPISODES = 100000
 
 
 class DQNAgent:
@@ -16,11 +22,12 @@ class DQNAgent:
         self.state_size = state_size
         self.action_size = action_size
         self.memory = deque(maxlen=2000)
-        self.gamma = 0.95    # discount rate
+        self.gamma = 0.95  # discount rate
         self.epsilon = 1.0  # exploration rate
-        self.epsilon_min = 0.01
-        self.epsilon_decay = 0.995
+        self.epsilon_min = 0.001
+        self.epsilon_decay = 0.996
         self.learning_rate = 0.001
+        self.observe = 0
         self.model = self._build_model()
 
     def _build_model(self):
@@ -44,6 +51,8 @@ class DQNAgent:
 
     def replay(self, batch_size):
         minibatch = random.sample(self.memory, batch_size)
+        state_batch = []
+        target_f_batch = []
         for state, action, reward, next_state, done in minibatch:
             target = reward
             if not done:
@@ -52,6 +61,10 @@ class DQNAgent:
             target_f = self.model.predict(state)
             target_f[0][action] = target
             self.model.fit(state, target_f, epochs=1, verbose=0)
+            # state_batch.append(state)
+            # target_f_batch.append(target_f)
+        # state_batch = np.reshape(state_batch, (batch_size, self.state_size))
+        # target_f_batch = np.reshape(target_f_batch, (batch_size, self.action_size))
         if self.epsilon > self.epsilon_min:
             self.epsilon *= self.epsilon_decay
 
@@ -63,32 +76,35 @@ class DQNAgent:
 
 
 if __name__ == "__main__":
-    game = Game()
-    state_size = game.cur_state().shape[0]
+    # env = gym.make('CartPole-v1')
+    env = Game()
+    state_size = env.cur_state().shape[0]
     action_size = 3
     agent = DQNAgent(state_size, action_size)
+    # agent.load("./save/cartpole-dqn.h5")
     done = False
     batch_size = 32
-    n = 0
-    total_reward = 0
+    reward_sum = 0
 
     for e in range(EPISODES):
-        state = game.cur_state()
+        state = env.cur_state()
         state = np.reshape(state, [1, state_size])
         for time in range(500):
-            game.render()
+            env.dealEvents()
+            env.render()
             action = agent.act(state)
-            next_state, reward, done = game.step(game.toAction(action))
-            game.dealEvent()
-            total_reward = (total_reward + reward) if time > 0 else total_reward
+            next_state, reward, done, _ = env.step(action)
+            # reward = reward if not done else -10
+            reward_sum += reward
+
             next_state = np.reshape(next_state, [1, state_size])
             agent.remember(state, action, reward, next_state, done)
             state = next_state
             if done:
-                print("episode: {}/{}, score: {}, e: {:.2}, total_reward: {}"
-                      .format(e, EPISODES, time, agent.epsilon, total_reward))
+                print("episode: {}/{}, score: {}, e: {:.2}"
+                      .format(e, EPISODES, reward_sum, agent.epsilon))
                 break
-            if len(agent.memory) > batch_size:
+            if len(agent.memory) > batch_size and e > agent.observe:
                 agent.replay(batch_size)
         # if e % 10 == 0:
         #     agent.save("./save/cartpole-dqn.h5")
--- dqn/game.py
+++ dqn/game.py
@@ -1,38 +1,53 @@
 # -*- encoding=utf-8 -*-
 import pygame
+import random
 from pygame.locals import *
-import config
-import sys
 import numpy as np
+import sys
 
+BLACK     = (0  ,0  ,0  )
+WHITE     = (255,255,255)
+SCREEN_SIZE = [60,110]
+BAR_SIZE = [10, 8]
+BALL_SIZE = [8, 8]
+#  神经网络的输出
+MOVE_STAY = 0 #[1, 0, 0]
+MOVE_LEFT = 1 #[0, 1, 0]
+MOVE_RIGHT = 2 #[0, 0, 1]
 class Game(object):
-    render_ed = False
     def __init__(self):
         pygame.init()
         self.clock = pygame.time.Clock()
-        self.screen = pygame.display.set_mode(config.SCREEN_SIZE)
-        pygame.display.set_caption('Simple Game')
+        self.render_ed = False
+        self.ball_pos_x = SCREEN_SIZE[0]//2 - BALL_SIZE[0]/2
+        self.ball_pos_y = SCREEN_SIZE[1]//2 - BALL_SIZE[1]/2
+        self.ball_dir_x = -1 # -1 = left 1 = right  
+        self.ball_dir_y = -1 # -1 = up   1 = down
+        self.ball_pos = pygame.Rect(self.ball_pos_x, self.ball_pos_y, BALL_SIZE[0], BALL_SIZE[1])
+        self.bar_pos_x = SCREEN_SIZE[0]//2-BAR_SIZE[0]//2
+        self.bar_pos = pygame.Rect(self.bar_pos_x, SCREEN_SIZE[1]-BAR_SIZE[1], BAR_SIZE[0], BAR_SIZE[1])
+        topl_x = SCREEN_SIZE[0] // 3
+        topl_y = 8
+        topl_sx = SCREEN_SIZE[0] // 3
+        topl_sy = 3
 
-        self.ball_pos_x = config.SCREEN_SIZE[0] // 2 - config.BALL_SIZE[0] / 2
-        self.ball_pos_y = config.SCREEN_SIZE[1] // 2 - config.BALL_SIZE[1] / 2
-
-        self.ball_dir_x = -1  # -1 = left 1 = right
-        self.ball_dir_y = -1  # -1 = up   1 = down
-        self.ball_pos = pygame.Rect(self.ball_pos_x, self.ball_pos_y, config.BALL_SIZE[0], config.BALL_SIZE[1])
-
-        self.bar_pos_x = config.SCREEN_SIZE[0] // 2 - config.BAR_SIZE[0] // 2
-        self.bar_pos = pygame.Rect(self.bar_pos_x, config.SCREEN_SIZE[1] - config.BAR_SIZE[1], config.BAR_SIZE[0], config.BAR_SIZE[1])
-
+        leftl_x = 8
+        leftl_y = SCREEN_SIZE[1] // 3
+        leftl_sx = 2
+        leftl_sy = SCREEN_SIZE[1] // 3
+        self.topl_pos = pygame.Rect(topl_x, topl_y, topl_sx, topl_sy)
+        self.leftl_pos = pygame.Rect(leftl_x, leftl_y, leftl_sx, leftl_sy)
+        #self.leftl_pos = pygame.Rect(self.ball_pos_x, self.ball_pos_y, BALL_SIZE[0], BALL_SIZE[1])
     # action是MOVE_STAY、MOVE_LEFT、MOVE_RIGHT
     # ai控制棒子左右移动；返回游戏界面像素数和对应的奖励。(像素->奖励->强化棒子往奖励高的方向移动)
     def render(self):
         if not self.render_ed:
-            self.screen = pygame.display.set_mode(config.SCREEN_SIZE)
+            self.screen = pygame.display.set_mode(SCREEN_SIZE)
             pygame.display.set_caption('Simple Game')
             self.render_ed = True
-        self.screen.fill(config.BLACK)
-        pygame.draw.rect(self.screen, config.WHITE, self.bar_pos)
-        pygame.draw.rect(self.screen, config.WHITE, self.ball_pos)
+        self.screen.fill(BLACK)
+        pygame.draw.rect(self.screen, WHITE, self.bar_pos)
+        pygame.draw.rect(self.screen, WHITE, self.ball_pos)
         '''
         if self.ball_dir_y == -1:
             pygame.draw.rect(self.screen, WHITE, self.topl_pos)
@@ -43,62 +58,38 @@ class Game(object):
         pygame.display.update()
         return screen_image
     def cur_state(self):
-        state = np.asarray([self.ball_pos.left, self.ball_pos.bottom, self.bar_pos.top, self.bar_pos_x, self.ball_dir_x,
-                            self.ball_dir_y])
+        state = np.asarray([self.ball_pos.left, self.ball_pos.bottom, self.bar_pos.top, self.bar_pos_x, self.ball_dir_x, self.ball_dir_y])
         return state
     def step(self, action):
-        if action == config.MOVE_LEFT:
+        if action == MOVE_LEFT:
             self.bar_pos_x = self.bar_pos_x - 2
-        elif action == config.MOVE_RIGHT:
+        elif action == MOVE_RIGHT:
             self.bar_pos_x = self.bar_pos_x + 2
         else:
             pass
         if self.bar_pos_x < 0:
             self.bar_pos_x = 0
-        if self.bar_pos_x > config.SCREEN_SIZE[0] - config.BAR_SIZE[0]:
-            self.bar_pos_x = config.SCREEN_SIZE[0] - config.BAR_SIZE[0]
-
-        self.screen.fill(config.BLACK)
+        if self.bar_pos_x > SCREEN_SIZE[0] - BAR_SIZE[0]:
+            self.bar_pos_x = SCREEN_SIZE[0] - BAR_SIZE[0]
         self.bar_pos.left = self.bar_pos_x
-        pygame.draw.rect(self.screen, config.WHITE, self.bar_pos)
-
         self.ball_pos.left += self.ball_dir_x * 2
         self.ball_pos.bottom += self.ball_dir_y * 3
-        pygame.draw.rect(self.screen, config.WHITE, self.ball_pos)
-
-        if self.ball_pos.top <= 0 or self.ball_pos.bottom >= (config.SCREEN_SIZE[1] - config.BAR_SIZE[1] + 1):
+        if self.ball_pos.top <= 0 or self.ball_pos.bottom >= (SCREEN_SIZE[1] - BAR_SIZE[1]+1):
             self.ball_dir_y = self.ball_dir_y * -1
-        if self.ball_pos.left <= 0 or self.ball_pos.right >= (config.SCREEN_SIZE[0]):
+        if self.ball_pos.left <= 0 or self.ball_pos.right >= (SCREEN_SIZE[0]):
             self.ball_dir_x = self.ball_dir_x * -1
-
         reward = 0
-        if self.bar_pos.top <= self.ball_pos.bottom and (
-                self.bar_pos.left < self.ball_pos.right and self.bar_pos.right > self.ball_pos.left):
-            reward = 1  # 击中奖励
-        elif self.bar_pos.top <= self.ball_pos.bottom and (
-                self.bar_pos.left > self.ball_pos.right or self.bar_pos.right < self.ball_pos.left):
-            reward = -1  # 没击中惩罚
-
-        # 获得游戏界面像素
-        screen_image = pygame.surfarray.array3d(pygame.display.get_surface())
-        pygame.display.update()
+        if self.bar_pos.top <= self.ball_pos.bottom and (self.bar_pos.left < self.ball_pos.right and self.bar_pos.right > self.ball_pos.left):
+            reward = 100    # 击中奖励
+        elif self.bar_pos.top <= self.ball_pos.bottom and (self.bar_pos.left > self.ball_pos.right or self.bar_pos.right < self.ball_pos.left):
+            reward = -100   # 没击中惩罚
         # 返回游戏界面像素和对应的奖励
-        # return screen_image, reward, reward != 0
-        return self.cur_state(), reward, reward != 0
+        return self.cur_state(), reward, reward != 0, None
 
-    def toAction(self, act):
-        if 0 == act:
-            return [0, 0, 0]
-        elif 1 == act:
-            return [0, 1, 0]
-        elif 2 == act:
-            return [0, 0, 1]
-        else:
-            return [0, 0, 0]
-
-    def dealEvent(self):
+    def dealEvents(self):
         for event in pygame.event.get():  # macOS需要事件循环，否则白屏
             if event.type == QUIT:
                 pygame.quit()
                 print("keyboard interrupt exit")
                 sys.exit()
+#  定义CNN-卷积神经网络 参考:http://blog.topspeedsnail.com/archives/10451
